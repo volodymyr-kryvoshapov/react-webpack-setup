810be62e9d927f41a66e9ef4c9fd2758aa6e7c81
volodymyr.k <volodymyr.k@playtika.com>
2022-04-22 16:29:54 +0300

    Use only css common approach

--- webpack.config.js
+++ webpack.config.js
@@ -27,25 +27,9 @@ module.exports = {
       {
         test: /\.css$/i,
         exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              importLoaders: 1,
-              modules: {
-                localIdentName: '[name]__[local]__[contenthash:base64:5]',
-                auto: resourcePath => resourcePath.endsWith('.m.css'),
-              },
-            },
-          },
-        ],
-      },
+        use: ["style-loader", "css-loader"],
+      }
     ]
   },
   plugins: [new HtmlWebpackPlugin({ template: path.join(__dirname, 'src', 'index.html') })],
-  // resolve: {
-  //   extensions: ['*', '.js', '.jsx'],
-  // },
 };
